2ced4332c8f796cd72dd3584bdadb38542ad8d5e
AdityaCricFan <dcadityakulkarni@gmail.com>
2023-01-05 08:22:04 +0530

    5Jan-IsGraphBipartite

---
+++ Graphs/isGraphBipartite.java
@@ -0,0 +1,26 @@
+class isGraphBipartite {
+    // DFS Graph Coloring Approach
+    // From the property of bipartite graph we can say that it has a chromatic value of 2
+    public boolean isBipartite(int[][] graph) {
+        // Make an array to contain the color associated with each node in the graph
+        int[] colorArray = new int[graph.length];
+        for(int i = 0; i < graph.length; i++){
+            if(colorArray[i] == 0 && !validColor(graph, colorArray, 1, i))
+                return false;
+        }
+        return true;    
+    }
+    public boolean validColor(int[][] graph, int[] colorArray, int color, int nodeCount){
+        // If node has already been visited, return true if color of the neighbouring node (that is, node calling the function) and this node are different else return false. Color conflict indicates violation of bipartite graph property
+        if(nodeArray[nodeCount] != 0)
+            return colorArray[nodeCount] == color;
+        // Color an uncolored node
+        colorArray[nodeCount] = color;
+        // For each of the neighbour, color with opposite color if neighbour not visited. If visited return false if there's a color conflict
+        for(int neighbour: graph[nodeCount]){
+            if(!validColor(graph, colorArray, -color, neighbour))
+                return false;
+        }
+        return true;
+    }
+}
